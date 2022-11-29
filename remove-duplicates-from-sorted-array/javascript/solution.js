11994c841563392ad203ed5704dffce8d4a26160
Fernando L. Ribeiro <fernandoribeiro@protonmail.com>
2022-11-29 18:31:21 -0300

    Remove unnecessary code

--- remove-duplicates-from-sorted-array/javascript/solution.js
+++ remove-duplicates-from-sorted-array/javascript/solution.js
@@ -1,14 +1,13 @@
 var removeDuplicates = function (nums) {
   let iterations = 0;
-  let insertIndex = 1;
+
   for (let i = 1; i < nums.length; i++) {
-    iterations++;
     if (nums[i - 1] != nums[i]) {
       nums[insertIndex] = nums[i];
       insertIndex++;
     }
   }
-  console.log(iterations);
+
   nums.splice(insertIndex, nums.length - insertIndex);
   return insertIndex;
 };
