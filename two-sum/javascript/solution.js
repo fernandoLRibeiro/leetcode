6296e816b91bd2aeb7e96d056644691291a85447
Fernando L. Ribeiro <fernandoribeiro@protonmail.com>
2023-02-28 20:04:51 -0300

    Optimize two-sum solution (javascript)

--- two-sum/javascript/solution.js
+++ two-sum/javascript/solution.js
@@ -1,12 +1,15 @@
 var twoSum = function (nums, target) {
-  for (let index1 = 0; index1 <= nums.length; index1++) {
-    for (let index2 = index1 + 1; index2 <= nums.length; index2++) {
-      if (nums[index1] + nums[index2] === target) {
-        return [index1, index2];
-      }
+  let hash = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    let diff = target - nums[i];
+
+    if (hash.hasOwnProperty(diff)) {
+      return [hash[diff], i];
     }
+    hash[nums[i]] = i;
   }
-  return `Numbers couldn't be found`;
+  return;
 };
 
 console.log(twoSum([2, 7, 11, 15], 9));
