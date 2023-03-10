7ace6d246a4f9978ebafd59bac1f6adcc21dd868
Fernando L. Ribeiro <fernandoribeiro@protonmail.com>
2023-03-10 20:12:30 -0300

    First working solution for Container With Most Water (javascript)

---
+++ container-with-most-water/javascript/solution.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  let maximumArea = 0;
+  let left = 0;
+  let right = height.length - 1;
+
+  while (left < right) {
+    const currentArea = Math.min(height[left], height[right]) * (right - left);
+
+    maximumArea = Math.max(maximumArea, currentArea);
+
+    if (height[left] > height[right]) {
+      right--;
+      continue;
+    }
+
+    left++;
+  }
+  return maximumArea;
+};
+
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
+// expect 49
+console.log(maxArea([1, 1]));
+// expect 1
