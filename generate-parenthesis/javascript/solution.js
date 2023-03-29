09f8a58bca3d91e01dafced31d1f9c0df1e11142
Fernando L. Ribeiro <fernandoribeiro@protonmail.com>
2023-03-29 10:30:33 -0300

    First working solution to Generate Parenthesis (javascript)

---
+++ generate-parenthesis/javascript/solution.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+
+var generateParenthesis = function (n) {
+  let stack = [];
+  let res = [];
+
+  function backtrack(openN, closedN) {
+    if (openN === n && closedN === n) {
+      res.push(stack.join(""));
+      return;
+    }
+
+    if (openN < n) {
+      stack.push("(");
+      backtrack(openN + 1, closedN);
+      stack.pop();
+    }
+    if (closedN < openN) {
+      stack.push(")");
+      backtrack(openN, closedN + 1);
+      stack.pop();
+    }
+  }
+
+  backtrack(0, 0);
+
+  return res;
+};
+
+console.log(generateParenthesis(3));
+console.log(generateParenthesis(4));
+console.log(generateParenthesis(2));
+console.log(generateParenthesis(1));
