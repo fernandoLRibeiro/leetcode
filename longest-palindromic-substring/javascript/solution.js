696a41392aa5ebc125e1715b2b14bb5f12ef427f
Fernando L. Ribeiro <fernandoribeiro@protonmail.com>
2023-07-28 09:36:18 -0300

    First working solution to 5. Longest Palindromic Substring (javascript)

---
+++ longest-palindromic-substring/javascript/solution.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function (s) {
+  let res = "";
+  resLen = 0;
+
+  for (let i = 0; i < s.length; ++i) {
+    let l = i;
+    let r = i;
+
+    while (l >= 0 && r < s.length && s[l] === s[r]) {
+      if (r - l + 1 > resLen) {
+        res = s.slice(l, r + 1);
+        resLen = res.length;
+      }
+
+      l--;
+      r++;
+    }
+
+    l = i;
+    r = i + 1;
+    while (l >= 0 && r < s.length && s[l] === s[r]) {
+      if (r - l + 1 > resLen) {
+        res = s.slice(l, r + 1);
+        resLen = res.length;
+      }
+
+      l--;
+      r++;
+    }
+  }
+
+  return res;
+};
