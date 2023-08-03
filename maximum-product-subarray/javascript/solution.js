3137ecb91ea70390a74bed685a1e2c027b67e81a
Fernando L. Ribeiro <fernandoribeiro@protonmail.com>
2023-08-03 10:29:00 -0300

    First working solution to 152. Maximum Product Subarray (javascript)

---
+++ maximum-product-subarray/javascript/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function (nums) {
+  let max = Math.max(...nums);
+  let currMin = 1;
+  let currMax = 1;
+
+  for (let n = 0; n < nums.length; ++n) {
+    const temp = currMax;
+    currMax = Math.max(nums[n] * temp, nums[n] * currMin, nums[n]);
+    currMin = Math.min(nums[n] * temp, nums[n] * currMin, nums[n]);
+    max = Math.max(currMax, max);
+  }
+  return max;
+};
