b09a2c598f33f42e5edd9d0e80056878eb8c6ee5
Fernando L. Ribeiro <fernandoribeiro@protonmail.com>
2023-08-06 10:48:34 -0300

    First working solution to 300. Longest Increasing Subsequence (javascript)

---
+++ longest-increasing-subsequence/javascript/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function (nums) {
+  let LIS = Array(nums.length).fill(1);
+
+  for (let i = nums.length - 1; i > -1; --i) {
+    for (let j = i + 1; j < nums.length; ++j) {
+      if (nums[i] < nums[j]) {
+        LIS[i] = Math.max(LIS[i], 1 + LIS[j]);
+      }
+    }
+  }
+
+  return Math.max(...LIS);
+};
