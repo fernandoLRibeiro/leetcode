8ecc89f7d300110651c4c89c59c5bd004b9b14b8
Fernando L. Ribeiro <fernandoribeiro@protonmail.com>
2023-08-31 11:22:40 -0300

    First working solution to 53. Maximum Subarray (JavaScript)

---
+++ maximum-subarray/javascript/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+  let maxSub = nums[0];
+  let curSum = 0;
+
+  for (let i = 0; i < nums.length; ++i) {
+    if (curSum < 0) {
+      curSum = 0;
+    }
+    curSum += nums[i];
+    maxSub = Math.max(maxSub, curSum);
+  }
+  return maxSub;
+};
