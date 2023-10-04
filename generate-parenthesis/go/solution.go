7dc17148b8ea77f868bf79c10b576819ff7480ae
Fernando L. Ribeiro <fernandoribeiro@protonmail.com>
2023-10-04 10:06:42 -0300

    First working solution to 22. Generate Parentheses (Go)

---
+++ generate-parenthesis/go/solution.go
@@ -0,0 +1,31 @@
+package main
+
+import "strings"
+
+func generateParenthesis(n int) []string {
+	var stack []string
+	var res []string
+
+	var backtrack func(int, int)
+	backtrack = func(openN int, closedN int) {
+
+		if openN == n && closedN == n {
+			res = append(res, strings.Join(stack, ""))
+			return
+		}
+
+		if openN < n {
+			stack = append(stack, "(")
+			backtrack(openN+1, closedN)
+			stack = (stack[:len(stack)-1])
+		}
+
+		if closedN < openN {
+			stack = append(stack, ")")
+			backtrack(openN, closedN+1)
+			stack = (stack[:len(stack)-1])
+		}
+	}
+	backtrack(0, 0)
+	return res
+}
